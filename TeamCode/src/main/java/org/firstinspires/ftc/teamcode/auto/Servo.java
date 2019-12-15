2d20688f3a6835895bac11ccd37da7d9eabe1631
Anas Saeed <saeed.anas@outlook.com>
2019-12-14 21:14:18 -0800

    DogeCv

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Servo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Servo.java
@@ -10,22 +10,14 @@ public class Servo extends Autonomous {
     public void runOpMode() {
         initHardware();
         if (opModeIsActive()) {
-            telemetry.addData("Value Left: ", 0.5);
-            telemetry.addData("Value Right: ", 0.5);
-            telemetry.update();
-            moveFoundation(0.5, 0.5);
-            sleep(1000);
-            telemetry.addData("Value Left: ", 0);
-            telemetry.addData("Value Right: ", 0);
-            telemetry.update();
-            moveFoundation(0, 0);
-            sleep(1000);
-            telemetry.addData("Value Left: ", 1);
-            telemetry.addData("Value Right: ", 1);
-            telemetry.update();
-            moveFoundation(1, 1);
-            sleep(1000);
+            for(int i = 0; i <= 1; i+=0.1) {
+                moveFoundation(i,i);
+                telemetry.addData("ServoVal: ", i);
+                telemetry.update();
+                sleep(1000);
+            }
         }
         // stop
     }
+
 }
